f54648289ddc592da2fe152c2e1226cb9142aba8
Erik <e.m.schepers@gmail.com>
2018-12-22 16:16:25 +0100

    Initial commit

--- gyro.py
+++ gyro.py
@@ -31,7 +31,7 @@ def dist(a,b):
  
 def get_y_rotation(x,y,z):
     radians = math.atan2(x, dist(y,z))
-    return -radians
+    return radians
  
 def get_x_rotation(x,y,z):
     radians = math.atan2(y, dist(x,z))
