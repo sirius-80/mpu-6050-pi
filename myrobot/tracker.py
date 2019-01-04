2c1d4db4d19e341bc6e42fce0d86b4a2562439d1
erik <e.m.schepers@gmail.com>
2019-01-04 20:57:23 +0100

    Fixed lag in tracker (cumulative delay caused by unnecessary sleep)

--- myrobot/tracker.py
+++ myrobot/tracker.py
@@ -1,16 +1,14 @@
 import logging
 import math
 import struct
-import time
 import threading
 
 
 class Tracker:
     """Location tracker. Once started will update its location every second.
     """
-    def __init__(self, pubsub_client=None, update_frequency=10.0):
+    def __init__(self, pubsub_client=None):
         self.pubsub_client = pubsub_client
-        self.update_frequency = update_frequency
         self.location = [0.0, 0.0]
         self.mouse_fd = open("/dev/input/mice", "rb")
         self.scale = 0.0001958033
@@ -45,10 +43,7 @@ class Tracker:
 
     def _update_location(self):
         """Read-out mouse data to update the current location."""
-        cycle_time = 1.0 / self.update_frequency
         while self.running:
-            start = time.monotonic()
-
             buf = self.mouse_fd.read(3)
             dx, dy = [float(i) * self.scale for i in struct.unpack("bb", buf[1:])]
             self.location[0] += dx
@@ -57,8 +52,3 @@ class Tracker:
 
             self.pubsub_client.send_location(*self.location)
             logging.debug("Location update. Now at (%.02f, %.02f)" % self.location)
-
-            # Determine next sleep period
-            remaining_cycle_time = cycle_time - (time.monotonic() - start)
-            if remaining_cycle_time > 0:
-                time.sleep(remaining_cycle_time)
