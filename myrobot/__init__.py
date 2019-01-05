722e89e55d2d6e46ff42a97f23d72cd1466e686b
erik <e.m.schepers@gmail.com>
2019-01-05 14:44:29 +0100

    Added different control strategies (command processing and backoff)

--- myrobot/__init__.py
+++ myrobot/__init__.py
@@ -8,8 +8,17 @@ import myrobot.gestures
 import myrobot.distance
 import myrobot.pubsub
 
+BACKOFF = 'backoff'
 
-class Robot(threading.Thread):
+COMMAND = 'command'
+
+
+class DistanceEventListener:
+    def distance_event(self, distance):
+        pass
+
+
+class Robot(threading.Thread, DistanceEventListener):
     """Creates a new robot. The robot takes gestures as commands, and uses an internal queue to process the commands
     into motor-commands (i.e. drive, turn)."""
     def __init__(self):
@@ -19,9 +28,12 @@ class Robot(threading.Thread):
         pubsub = myrobot.pubsub.PubSubClient()
         self.tracker = myrobot.tracker.Tracker(pubsub)
         self.gestures = myrobot.gestures.GestureReceiver(self.command_queue)
-        self.distance_device = myrobot.distance.DistanceDevice(pubsub)
+        self.distance_device = myrobot.distance.DistanceDevice(self.distance_event, pubsub)
         self.running = False
         self.emergency = False
+        self.strategies = {COMMAND: CommandStrategy(self, self.command_queue),
+                           BACKOFF: BackoffStrategy(self)}
+        self.strategy = self.strategies[COMMAND]
         self.distance_device.set_action_on_min_distance(self.emergency_break, 0.10)  # Emergency break at 0.10 m.
 
     def start(self):
@@ -30,6 +42,14 @@ class Robot(threading.Thread):
         self.tracker.start()
         self.distance_device.start()
         threading.Thread.start(self)
+
+    def distance_event(self, distance):
+        if distance < 0.1 and self.strategy is not self.strategies[BACKOFF]:
+            logging.warning("Emergency break!")
+            self.emergency = True
+            self.strategy = BackoffStrategy(self)
+        elif self.strategy is not self.strategies[COMMAND]:
+            self.strategy = CommandStrategy(self, self.command_queue)
 
     def emergency_break(self):
         logging.warning("Emergency break!")
@@ -42,29 +62,30 @@ class Robot(threading.Thread):
     def run(self):
         while self.running:
             self.emergency = False
-            logging.info("Waiting for command...")
-            command = self.command_queue.get()
-            logging.info("Processing command: [%s]" % (command, ))
-            if command == "forward":
-                self.tracker.reset()
-                self.motor.forward()
-                while not self.emergency and self.tracker.get_distance() < 0.5:
-                    logging.debug("Traveling forward: [%.2f / %.2f m.]" % (self.tracker.get_distance(), 0.5))
-                    time.sleep(0.1)
-                self.motor.stop()
-                logging.info("Traveled %.2f , forward.]" % (self.tracker.get_distance(), ))
-            elif command == "backward":
-                self.tracker.reset()
-                self.motor.backward()
-                while self.tracker.get_distance() < 0.5:
-                    logging.debug("Traveling backward: [%.2f / %.2f m.]" % (self.tracker.get_distance(), 0.5))
-                    time.sleep(0.1)
-                self.motor.stop()
-                logging.info("Traveled %.2f , backward.]" % (self.tracker.get_distance(), ))
-            elif command == "left":
-                self.motor.turn_left()
-            elif command == "right":
-                self.motor.turn_right()
+            self.control_strategy.execute()
+            # logging.info("Waiting for command...")
+            # command = self.command_queue.get()
+            # logging.info("Processing command: [%s]" % (command, ))
+            # if command == "forward":
+            #     self.tracker.reset()
+            #     self.motor.forward()
+            #     while not self.emergency and self.tracker.get_distance() < 0.5:
+            #         logging.debug("Traveling forward: [%.2f / %.2f m.]" % (self.tracker.get_distance(), 0.5))
+            #         time.sleep(0.1)
+            #     self.motor.stop()
+            #     logging.info("Traveled %.2f , forward.]" % (self.tracker.get_distance(), ))
+            # elif command == "backward":
+            #     self.tracker.reset()
+            #     self.motor.backward()
+            #     while self.tracker.get_distance() < 0.5:
+            #         logging.debug("Traveling backward: [%.2f / %.2f m.]" % (self.tracker.get_distance(), 0.5))
+            #         time.sleep(0.1)
+            #     self.motor.stop()
+            #     logging.info("Traveled %.2f , backward.]" % (self.tracker.get_distance(), ))
+            # elif command == "left":
+            #     self.motor.turn_left()
+            # elif command == "right":
+            #     self.motor.turn_right()
         logging.info("Stopping all robot parts...")
         self.motor.stop()
         self.tracker.stop()
@@ -72,3 +93,69 @@ class Robot(threading.Thread):
 
         self.motor.close()
         self.distance_device.close()
+
+
+class ControlStrategy:
+    def __init__(self, robot):
+        self.robot = robot
+        self.interrupt = False
+
+    def execute(self):
+        """Execute single pass of this strategy."""
+        self.interrupt = False
+
+    def interrupt(self):
+        self.interrupt = True
+
+
+class BackoffStrategy(ControlStrategy):
+    def __init__(self, robot):
+        ControlStrategy.__init__(self, robot)
+
+    def execute(self):
+        ControlStrategy.execute(self)
+        self.robot.motor.backward(50)
+        while not self.interrupt and self.robot.distance_device.get_distance() < 0.10:
+            time.sleep(0.1)
+        self.robot.motor.stop()
+
+
+class CommandStrategy(ControlStrategy):
+    def __init__(self, robot, command_queue):
+        ControlStrategy.__init__(self, robot)
+        self.command_queue = command_queue
+
+    def interrupt(self):
+        self.interrupt = True
+
+    def execute(self):
+        ControlStrategy.execute(self)
+        logging.info("Waiting for command...")
+        command = None
+        while not command:
+            command = self.command_queue.get(timeout=0.1)
+            if self.interrupt:
+                return
+
+        self.robot.emergency = False
+        logging.info("Processing command: [%s]" % (command, ))
+        if command == "forward":
+            self.robot.tracker.reset()
+            self.robot.motor.forward()
+            while not self.interrupt and self.robot.tracker.get_distance() < 0.5:
+                logging.debug("Traveling forward: [%.2f / %.2f m.]" % (self.robot.tracker.get_distance(), 0.5))
+                time.sleep(0.1)
+            logging.info("Traveled %.2f , forward.]" % (self.robot.tracker.get_distance(), ))
+        elif command == "backward":
+            self.robot.tracker.reset()
+            self.robot.motor.backward()
+            while not self.interrupt and self.robot.tracker.get_distance() < 0.5:
+                logging.debug("Traveling backward: [%.2f / %.2f m.]" % (self.robot.tracker.get_distance(), 0.5))
+                time.sleep(0.1)
+            logging.info("Traveled %.2f , backward.]" % (self.robot.tracker.get_distance(), ))
+        elif command == "left":
+            self.robot.motor.turn_left()
+        elif command == "right":
+            self.robot.motor.turn_right()
+        self.robot.motor.stop()
+
